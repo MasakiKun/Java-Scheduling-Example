a5fce11843a587c5ba6447425e877ec7e6c78277
ayukawa <kidconan@gmail.com>
2018-09-08 15:19:52 +0900

    최초 실행 후 딜레이를 2초에서 3초로 증가

--- Java-Timer/src/main/java/kr/ayukawa/javatimer/EntryPoint.java
+++ Java-Timer/src/main/java/kr/ayukawa/javatimer/EntryPoint.java
@@ -8,7 +8,7 @@ import java.util.Timer;
 public class EntryPoint {
 	public static void main(String[] args) {
 		System.out.println("현재시각: " + LocalDateTime.now().toString());
-		System.out.println("2초 후 시각 표시 시작");
+		System.out.println("3초 후 시각 표시 시작");
 
 		Timer timer = new Timer();
 		// 작업을 스케줄링하기 위해 schedule() 메서드를 사용한다
@@ -18,7 +18,7 @@ public class EntryPoint {
 				// 실행할 작업의 인스턴스
 				new JobTimer(),
 				// delay. 여기서 지정한 시간 이후에 작업이 실행된다
-				2000,
+				3000,
 				// 작업이 실행될 인터벌. 이 시간 간격으로 작업이 실행된다
 				1000
 		);
